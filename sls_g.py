3bc971ec5bf295906aa674ca900e98c7d6c5d9f0
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2019-05-09 18:32:40 +0200

    change label and node names

--- sls_g.py
+++ sls_g.py
@@ -10,55 +10,55 @@ labels = {}
 p=4
 
 for i in range(p):
-	n_name="B{}-{}".format(i, 0)
-	labels[n_name] = "B{}".format(i)
+	n_name="B-{}-{}".format(i, 0)
+	labels[n_name] = "B-{}".format(i)
 	G2.node(n_name, labels[n_name], color = 'black', style='filled', fontcolor='white')
 	for j in range(p):
-		n_name="A{}-{}-{}".format(i, j, 0)
-		labels[n_name] = "A{}-{}".format(i, j)
+		n_name="A-{}-{}-{}".format(i, j, 0)
+		labels[n_name] = "A-{}-{}".format(i, j)
 		G2.node(n_name, labels[n_name], color = 'black', style='filled', fontcolor='white')
 	
 for k in range(0, p):
-	n_name="inv{}".format(k)
-	labels[n_name] = "A{}-{}".format(k, k)
+	n_name="A-{}-{}-{}".format(k, k, k+1)
+	labels[n_name] = "A-{}-{}".format(k, k)
 	G2.node(n_name, labels[n_name], color = 'red', style='filled', fontcolor='white')
-	G2.edge("A{}-{}-{}".format(k, k, k), n_name)
+	G2.edge("A-{}-{}-{}".format(k, k, k), n_name)
 
-	n_name="B{}-{}".format(k, k+1)
-	labels[n_name] = "B{}".format(k)
+	n_name="B-{}-{}".format(k, k+1)
+	labels[n_name] = "B-{}".format(k)
 	G2.node(n_name, labels[n_name], color = 'magenta', style='filled', fontcolor='white')
-	G2.edge("inv{}".format(k), n_name)
-	G2.edge("B{}-{}".format(k, k), n_name)
+	G2.edge("A-{}-{}-{}".format(k, k, k+1), n_name)
+	G2.edge("B-{}-{}".format(k, k), n_name)
 	
 	for i in range(k+1, p):
-		n_name="A{}-{}-{}".format(k, i, k+1)
-		labels[n_name] = "A{}-{}".format(k, i)
+		n_name="A-{}-{}-{}".format(k, i, k+1)
+		labels[n_name] = "A-{}-{}".format(k, i)
 		G2.node(n_name, labels[n_name], color = 'blue', style='filled', fontcolor='white')
-		G2.edge("inv{}".format(k), n_name)
-		G2.edge("A{}-{}-{}".format(k, i, k), n_name)
+		G2.edge("A-{}-{}-{}".format(k, k, k+1), n_name)
+		G2.edge("A-{}-{}-{}".format(k, i, k), n_name)
 	
 	for i in range(k+1, p):
-		n_name="B{}-{}".format(i, k+1)
-		labels[n_name] = "B{}".format(i)
+		n_name="B-{}-{}".format(i, k+1)
+		labels[n_name] = "B-{}".format(i)
 		G2.node(n_name, labels[n_name], color = 'olivedrab2', style='filled', fontcolor='white')
-		G2.edge("B{}-{}".format(i, k), n_name)
-		G2.edge("B{}-{}".format(k, k+1), n_name)
-		G2.edge("A{}-{}-{}".format(i, k, k), n_name)
+		G2.edge("B-{}-{}".format(i, k), n_name)
+		G2.edge("B-{}-{}".format(k, k+1), n_name)
+		G2.edge("A-{}-{}-{}".format(i, k, k), n_name)
 		for j in range(k+1, p):
-			n_name="A{}-{}-{}".format(i, j, k+1)
-			labels[n_name] = "A{}-{}".format(i, j)
+			n_name="A-{}-{}-{}".format(i, j, k+1)
+			labels[n_name] = "A-{}-{}".format(i, j)
 			G2.node(n_name, labels[n_name], color = 'darkgreen', style='filled', fontcolor='white')
-			G2.edge("A{}-{}-{}".format(i, j, k), n_name)
-			G2.edge("A{}-{}-{}".format(i, k, k), n_name)
-			G2.edge("A{}-{}-{}".format(k, j, k+1), n_name)
+			G2.edge("A-{}-{}-{}".format(i, j, k), n_name)
+			G2.edge("A-{}-{}-{}".format(i, k, k), n_name)
+			G2.edge("A-{}-{}-{}".format(k, j, k+1), n_name)
 
 for k in range(1, p):
 	for i in range(0, p-k):
-		n_name="B{}-{}".format(i, k+1+i)
-		labels[n_name] = "B{}".format(i)
+		n_name="B-{}-{}".format(i, k+1+i)
+		labels[n_name] = "B-{}".format(i)
 		G2.node(n_name, labels[n_name], color = 'olivedrab2', style='filled', fontcolor='white')
-		G2.edge("B{}-{}".format(i, k+i), n_name)
-		G2.edge("B{}-{}".format(p-k, p), n_name)
-		G2.edge("A{}-{}-{}".format(i, p-k, i+1), n_name)
+		G2.edge("B-{}-{}".format(i, k+i), n_name)
+		G2.edge("B-{}-{}".format(p-k, p), n_name)
+		G2.edge("A-{}-{}-{}".format(i, p-k, i+1), n_name)
 	
 print(G2.source)
