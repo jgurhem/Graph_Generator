d17c3e8788636539afcf1461eeb723ca74956bb5
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2019-07-01 09:42:19 +0200

    Use the other LU algorithm

--- lu.py
+++ lu.py
@@ -15,8 +15,8 @@ for i in range(p):
 for k in range(0, p - 1):
 	myG.op_matrix_inv(gh.Node("A", [k, k, k]), gh.NodeSimple("inv", [k]))
 	for i in range(k+1, p):
-		myG.op_pmm1(gh.Node("A", [k, i, k]), gh.NodeSimple("inv", [k]))
+		myG.op_pmm1(gh.Node("A", [i, k, k]), gh.NodeSimple("inv", [k]))
 		for j in range(k+1, p):
-			myG.op_pmm_d(gh.Node("A", [i, k, k]), gh.Node("A", [k, j, k + 1]), gh.Node("A", [i, j, k]))
+			myG.op_pmm_d(gh.Node("A", [i, k, k + 1]), gh.Node("A", [k, j, k]), gh.Node("A", [i, j, k]))
 
 myG.graph_print()
